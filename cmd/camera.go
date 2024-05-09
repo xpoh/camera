3be8cd43f5a78746b72576e155db7133fc5c13fc
Dmitriy Rekunov <d.rekunov@rdp.ru>
2024-05-09 13:08:30 +0300

    [C] refactor

---
+++ cmd/camera.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	"github.com/bluenviron/gortsplib/v4"
+	"github.com/bluenviron/gortsplib/v4/pkg/base"
+)
+
+// This example shows how to
+// 1. connect to a RTSP server
+// 2. get and print informations about medias published on a path.
+
+func main() {
+	c := gortsplib.Client{}
+
+	u, err := base.ParseURL("rtsp://localhost:8554/mypath")
+	if err != nil {
+		panic(err)
+	}
+
+	err = c.Start(u.Scheme, u.Host)
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	desc, _, err := c.Describe(u)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("available medias: %v\n", desc.Medias)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
-module gituhb.com/xpoh/camera
+module github.com/xpoh/camera
 
 go 1.22.2
+
+require github.com/bluenviron/gortsplib/v4 v4.9.0
+
+require (
+	github.com/bluenviron/mediacommon v1.10.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtcp v1.2.14 // indirect
+	github.com/pion/rtp v1.8.7-0.20240429002300-bc5124c9d0d0 // indirect
+	github.com/pion/sdp/v3 v3.0.9 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
